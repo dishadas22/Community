19b21439d2bb75110717a6384cb6938597af29c0
Sainik Khaddar <73096052+playboySeductor@users.noreply.github.com>
2024-04-05 20:50:48 +0000

    changes and bugs fixed

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-var btn=document.queryselector('.hambtn');
-var btnshow=true;
-var menushow=document.getElementsByClassName("hamburger-menu");
-btn.onclick()= function(){
-    if(btnshow==true){
-        menushow.style.display=flex;
-        btnshow=false;
+// document.addEventListener('DOMContentLoaded', function() {
+//   const hamburgerBtn = document.getElementById('hambtn');
+//   const menu = document.getElementById('hamburger-menu');
 
-    }
-    else if(btnshow==false){
-        menushow.style.display=none;
-        btnshow=true;
-    }
+//   hamburgerBtn.addEventListener('click', function() {
+//     menu.classList.toggle('show');
+//   });
+// });
+
+const menu = document.querySelector(".hamburger-menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".hambtn");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
 }
+
+hamburger.addEventListener("click", toggleMenu);
+
+//const menuItems = document.querySelectorAll(".menuItem");
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
